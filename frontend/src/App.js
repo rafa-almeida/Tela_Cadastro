03dfb299da770089ae007b9462a504de1110748b
rafa-almeida <rafasegt@gmail.com>
2023-11-03 21:11:23 -0300

    adicionando back, api e mysql

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import GlobalStyle from "./styles/global"
+import styled from "styled-components"
+import Form from "./components/Form"
+import Grid from ".components/Grid"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2`
+
+`;
+
+
+
+function App() {
+  return (
+    <>
+    <Container>
+      <Title>Usuários</Title>
+      <Form />
+      <Grid />
+    </Container>
+    <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
